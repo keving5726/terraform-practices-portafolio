45feaf0b185d2451a3b0d985cac26897cee13f3e
Kevin González <keving5726@gmail.com>
2026-09-05 20:55:08 -0400

    Add networking configuration

--- azure/web-server-vm/main.tf
+++ azure/web-server-vm/main.tf
@@ -15,3 +15,44 @@ resource "azurerm_resource_group" "main" {
 
   tags = local.default_tags
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${local.prefix}-001"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = ["172.16.0.0/16"]
+
+  tags = local.default_tags
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "snet-${local.prefix}-001"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["172.16.0.0/24"]
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "pip-${local.prefix}-001"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+
+  tags = local.default_tags
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "nic-${local.prefix}-001"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  tags = local.default_tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_version    = "IPv4"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
